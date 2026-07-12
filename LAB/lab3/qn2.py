5e4a562e5279134e70e75e0e8df2f0ad8cacf78b
Swayam <stha.bhim1971@gmail.com>
2026-07-12 20:49:15 +0545

    codes for merge_sort

---
+++ LAB/lab3/qn2.py
@@ -0,0 +1,34 @@
+def merge_sort(A,l,r):
+    if l<r:
+        m = int((l+r)/2)
+        print(m)
+        merge_sort(A,l,m)
+        merge_sort(A,m+1,r)
+        merge(A,l,m+1,r)
+    return A
+def merge(A,l,m,r):
+    i  = l
+    j = r
+    k = m
+    B = []
+    while (i<m and k<=j ):
+        if (A[i]<A[k]):
+            B.append(A[i])
+            i+=1
+        else:
+            B.append(A[k])        
+            k+=1
+    while (i < m):
+        B.append(A[i]) 
+        i+=1
+    while (k<=j):
+        B.append(A[k])    
+        k+=1
+    for index in range(len(B)):
+        A[l+index] = B[index]
+        print(A)
+    return A
+A=[1,2,3,4,5,623,22,234,5532,2,43]
+print(len(A))
+sorted = merge_sort(A,0,len(A)-1)
+print(sorted)
